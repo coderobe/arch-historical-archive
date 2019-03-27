6638bc3a3e3954a5309836d5ac52e0965db6bed4
Florian Pritz <bluewind@xinu.at>
2019-03-28 00:12:27 +0100

    test: Verify precondition
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- test.py
+++ test.py
@@ -12,6 +12,11 @@ class TestUploader(unittest.TestCase):
     def test_upload_pkg(self):
         mock_uploader = MagicMock()
         app = upload_pkg_internetarchive.ArchiveUploader(mock_uploader, DB.DB(':memory:'))
+
+        self.assertFalse(app.db.exists('fb-client-2.0.4-1-any.pkg.tar.xz'))
+        self.assertFalse(app.db.exists('fb-client-2.0.3-2-any.pkg.tar.xz'))
+        self.assertFalse(app.db.exists('fb-client-2.0.99-1-any.pkg.tar.xz'))
+
         app.main('./test-data/archive/packages/f/fb-client')
         app.main('./test-data/archive/packages/f/fb-client')
 
