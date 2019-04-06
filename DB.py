9c17dead90a674e92a23973e6065929a771d661e
Florian Pritz <bluewind@xinu.at>
2019-04-06 20:37:05 +0200

    Add primary key to table
    Oops. There should have been an index there... Having it should improve
    performance somewhat.
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- DB.py
+++ DB.py
@@ -7,8 +7,27 @@ class DB:
 
     def init_db(self):
         c = self.db.cursor()
-        c.execute('create table if not exists files (filename text, uploaded int)')
+        version = self._get_version()
+
+        if version is None:
+            c.execute('create table if not exists files (filename text, uploaded int)')
+            self._set_version(1)
+            version = 1
+
+        if version < 2:
+            c.execute('alter table files rename to files_old;')
+            c.execute('create table files (filename text, uploaded int, primary key (`filename`)) without rowid;')
+            c.execute('insert into files select * from files_old;')
+            c.execute('drop table files_old;')
+            self._set_version(2)
+
         self.db.commit()
+
+    def _get_version(self):
+        self.db.cursor().execute('pragma user_version').fetchone()[0]
+
+    def _set_version(self, version):
+        self.db.cursor().execute('pragma user_version='+str(version))
 
     def add_file(self, filename):
         c = self.db.cursor()
